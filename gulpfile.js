264a68efc8af14942b19435c2977aedac9648e2a
Tigran Kaginyan <6granik@gmail.com>
2016-11-15 00:50:58 +0300

    Сборка картинок и стили для баннера

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,14 @@ var autoprefixer = require('gulp-autoprefixer');
 var minifyCss = require('gulp-clean-css');
 
 
-gulp.task('default', function() {
+gulp.task('default', ['images'], function() {
   return gulp.src('src/css/styles.css')
     .pipe(autoprefixer('last 2 version'))
     .pipe(minifyCss())
     .pipe(gulp.dest('public/css'))
 });
+
+gulp.task('images', function(){
+  return gulp.src('src/images/*')
+    .pipe(gulp.dest('public/images'))
+})
